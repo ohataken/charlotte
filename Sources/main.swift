39b81f3134a4853ad56c92390a2051cb1f6611dc
ohataken <ohataken@outlook.com>
2017-01-31 20:48:46 +0900

    update main

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,6 +5,11 @@ let args = ProcessInfo.processInfo.arguments
 let user = args[1]
 let pass = args[2]
 let host = args[3]
+let ssid = args[4]
+
+if ssid != getSSID() {
+    exit(0)
+}
 
 let updater = NoIPUpdater(user: user, pass: pass, host: host)
 
