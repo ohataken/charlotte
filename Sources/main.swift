f504be8d1744cfb2a7403cadf08321e207d09292
ohataken <ohataken@outlook.com>
2017-01-31 20:32:00 +0900

    update main

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import Foundation
+import Alamofire
+
+let args = ProcessInfo.processInfo.arguments
+let user = args[1]
+let pass = args[2]
+let host = args[3]
+
+let updater = NoIPUpdater(user: user, pass: pass, host: host)
+
+Alamofire.request(updater.getURLString()).response { (response) in
+    print(response.response!)
+    exit(0)
+}
+
+RunLoop.main.run()
