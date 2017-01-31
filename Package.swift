9cd10abbaa2a460672c59dbb35a66bf296bb829a
ohataken <ohataken@outlook.com>
2017-01-31 19:39:00 +0900

    add Alamofire to dependencies

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "charlotte"
+    name: "charlotte",
+    dependencies: [
+        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
+    ]
 )
